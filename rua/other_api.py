eb6331357efa3337f65c49a085f1530bf60ef075
Zeta-qixi <1019289695@qq.com>
2021-10-18 16:33:12 +0800

    debug

--- rua/other_api.py
+++ rua/other_api.py
@@ -15,7 +15,8 @@ try:
 except:
     master = []
 print(master)
-api_list ={
+
+api_dict ={
     '爬': 'pa',
     '撕了': 'si',
     '吃了': 'chi',
@@ -24,13 +25,12 @@ api_list ={
 pa = on_command('爬', aliases={'撕了', '吃了'})
 @pa.handle()
 async def pa_handle(bot:Bot, event: MessageEvent):
-    print(event.__dict__)
-    try:
-        msg = str(event.message)
-        comm = msg.split('[')[0]
-        qq = int(re.search(r"[\[CQ:at,qq=]([0-9].{0,20})[\]]", msg).group(1))
+
+        msg = str(event.raw_message)
+        ret = re.search(r"(.*)?\[CQ:at,qq=(.*)\]", msg)
+        comm = str(ret.group(1))
+        qq = int(ret.group(2))
         if qq in master:
-            qq = event.sender_id
-        await bot.send(event, message = Message(f"[CQ:image,file=http://ovooa.com/API/{api_list[comm]}/api.php?QQ={qq}]"))
-    except:
-        pass
+            qq = event.user_id
+        await bot.send(event, message = Message(f"[CQ:image,file=http://ovooa.com/API/{api_dict[comm]}/api.php?QQ={qq}]"))
+
