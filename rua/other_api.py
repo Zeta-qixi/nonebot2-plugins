50c0ccce8d047364c511f9b75108ce4944f37fa4
Zeta-qixi <1019289695@qq.com>
2021-10-19 17:18:36 +0800

    更多策略

--- rua/other_api.py
+++ rua/other_api.py
@@ -3,41 +3,40 @@
 http://ovooa.com
 '''
 import re
-from nonebot import on_command, get_driver
+from nonebot import on_command, get_driver, on_regex
 from nonebot.adapters.cqhttp.bot import Bot
 from nonebot.adapters.cqhttp.event import MessageEvent
 from nonebot.adapters.cqhttp.message import Message, MessageSegment
 from nonebot.typing import T_State
 
-
 try:
     master = get_driver().config.master
 except:
     master = []
-print(master)
 
 api_dict ={
     '爬': 'pa',
     '撕了': 'si',
     '吃了': 'chi',
 }
-
-pa = on_command('爬', aliases={'撕了', '吃了'})
+keywords = ''
+for k in api_dict:
+    if keywords == '':
+        keywords = k
+    else:
+        keywords = keywords + '|' + k
+pa = on_regex(f"({keywords})?(.*)")
 @pa.handle()
-async def pa_handle(bot:Bot, event: MessageEvent):
+async def pa_handle(bot:Bot, event: MessageEvent, state: T_State):
+    regex = (state['_matched_groups'])
+    comm = regex[0]
 
-        msg = str(event.raw_message)
-        print(msg)
-        try:
-            ret = re.search(r"(.*)?\[CQ:at,qq=(.*)\]", msg)
-            comm = str(ret.group(1))
-            qq = int(ret.group(2))
-        except:
-            ret = msg.split()
-            comm = ret[0]
-            qq = ret[1]
-            
+    if comm:
+        qq_ = regex[1].split()[0]
+        if qq_.isdigit():
+            qq = int(qq_)
+        elif 'CQ:at' in qq_:
+            qq = re.search(r"\[CQ:at,qq=(.*)\]", qq_).group(1)
         if qq in master:
             qq = event.user_id
         await bot.send(event, message = Message(f"[CQ:image,file=http://ovooa.com/API/{api_dict[comm]}/api.php?QQ={qq}]"))
-
