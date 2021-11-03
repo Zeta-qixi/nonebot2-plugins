fbd8d885d34af59762fd7e83f9c53cb3cc6e5772
Zeta-qixi <1019289695@qq.com>
2021-11-03 09:49:21 +0800

    不再使用nickname

--- jrrp.py
+++ jrrp.py
@@ -8,12 +8,11 @@ import time
 import random
 import nonebot
 from nonebot import require
+
 scheduler = require('nonebot_plugin_apscheduler').scheduler
-from . import nickname
-nn = nickname.nickname()
 master = nonebot.get_driver().config.master
 
-class rp:
+class RPBot:
     def __init__(self):
         self.rp = {}
 
@@ -22,12 +21,12 @@ class rp:
             return self.rp[num]
         else:
             if num in master:
-                self.rp[num] = random.randrange(51) +50
-                return self.rp[num]
-            self.rp[num] = random.randrange(101)
+                self.rp[num] = random.randrange(51) + 50
+            else:
+                self.rp[num] = random.randrange(101)
             return self.rp[num]
 
-rpbot = rp()
+rpbot = RPBot()
 
 jrrp = on_command('jrrp')
 @jrrp.handle()
@@ -35,16 +34,9 @@ async def _(bot: Bot, event: GroupMessageEvent):
     msg = event.raw_message
     user_id = event.user_id
     jp = rpbot.RP(user_id)
-
-    if jp:
-        msg= f'今天的人品值是:{jp}'
-        if nn.my_name(user_id):
-            await bot.send(event, message=f'{nn.my_name(user_id)}{msg}')
-        else:
-            await bot.send(event, message=MessageSegment.at(user_id)+msg)
+    msg= f'今天的人品值是:{jp}'
+    await bot.send(event, message=MessageSegment.at(user_id)+msg)
     
-
-
 @scheduler.scheduled_job('cron', hour='0', minute='0', second='0', misfire_grace_time=60) # = UTC+8 1445
 async def clean():
     rpbot.rp = {}
