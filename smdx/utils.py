874a7c0a61e978f03cc11427c9f695539bfd07ea
Zeta-qixi <1019289695@qq.com>
2021-11-16 10:35:18 +0800

    修改一下正则

--- smdx/utils.py
+++ smdx/utils.py
@@ -1,20 +1,2 @@
 
 
-import datetime
-import re
-# 今天是几号星期
-WEEK = ['一', '二', '三', '四', '五', '六', '日']
-def get_weekday(date: str):
-    print(date)
-    if date[:2] == '今天':
-        index = datetime.date.today().weekday()
-    elif date[:2] == '明天':
-        index = datetime.date.today().weekday()+1
-    else:
-        ret = re.search('(\d{4})?[-.年]?(\d{1,2})[-.月](\d{1,2})[日号]?', date)
-        year, mouth, day = ret.groups()
-        if not year:
-            year = datetime.date.today().year
-        index = datetime.date(int(year),int(mouth),int(day)).weekday()
-
-    return WEEK[index]
