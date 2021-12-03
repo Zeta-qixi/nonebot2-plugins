efb4e574002c3d11428ef729576481cfdd1bd6e6
Zeta-qixi <1019289695@qq.com>
2021-12-03 09:36:57 +0800

    新功能

--- games/tools.py
+++ games/tools.py
@@ -1,5 +1,7 @@
-from random import shuffle, choice
+from random import choice, shuffle
+
 from nonebot.adapters.cqhttp.message import Message
+
 
 class RouletteGame:
     '''
@@ -14,14 +16,22 @@ class RouletteGame:
         self.set_bullet(0)
         self.filled = False
         self.member = []
-        self.rule = '---指令列表---\n【填装（数字）】填装子弹\n【开枪】开一枪\n【结束】献祭一个🐎结束游戏'
+        self.rule = '---指令列表---\n【填装（数字）】填装子弹\n【开枪】开一枪\n【决斗】 @一个人 开启solo模式\n【结束】献祭一个🐎结束游戏'
         self.bullet = 0
+        self.duel = False
 
     def set_bullet(self, nums):
         self.revolver = [0 for i in range(6)]
         for i in range(nums):
             self.revolver[i] = 1
         shuffle(self.revolver)
+        self.member = []
+        self.duel = False
+
+
+    def set_duel(self, id, id2):
+        self.member = [id, id2]
+        self.duel = True
 
     def shoot(self):
         res = self.revolver[0]
@@ -32,8 +42,10 @@ class RouletteGame:
         times = len(self.revolver)
         bullet = sum(self.revolver)
 
-        return(times, bullet)
+        return(times, bullet, self.duel)
 
+    def get_member(self):
+        return self.member
     def dead(self, id):
         pass
         #self.member[id] = 'dead'
