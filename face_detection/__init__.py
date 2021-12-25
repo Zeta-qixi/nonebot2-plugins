c4e71bb9166e3d224f6a178fba80777a0aa79db9
Zeta-qixi <1019289695@qq.com>
2021-12-25 18:49:10 +0800

    mediapipe来咯

---
+++ face_detection/__init__.py
@@ -0,0 +1,24 @@
+from .detection import IMAGE_PATH, get_detection_res
+
+from nonebot import on_command
+from nonebot.adapters.cqhttp.bot import Bot
+from nonebot.adapters.cqhttp.event import Event, GroupMessageEvent,MessageEvent
+from nonebot.adapters.cqhttp.message import MessageSegment
+from nonebot.typing import T_State
+from nonebot import require, logger
+
+face_ = on_command("检测")
+
+@face_.handle()
+async def add_handle(bot: Bot, event: Event, state: T_State):
+    pass
+    for msg in event.get_message():
+        if msg.type == 'image':
+            url = msg.data['url']
+            
+            if(get_detection_res(url)):
+                logger.info("检测完成")
+                msg_pic = MessageSegment.image(f"file://{IMAGE_PATH}")
+                await face_.finish(message=msg_pic)
+            else:
+                logger.info("检测不到东西噢")
---
+++ face_detection/detection.py
@@ -0,0 +1,38 @@
+import cv2
+import mediapipe
+import os
+
+from PIL import Image
+from io import BytesIO
+import requests
+
+
+PATH =os.path.dirname(__file__)+'/data/'
+IMAGE_PATH = PATH + 'out.png'
+IMAGE_INPUT =  PATH + 'in.png'
+mp_face_detection = mediapipe.solutions.face_detection
+mp_drawing = mediapipe.solutions.drawing_utils
+
+
+
+def get_detection_res(url):
+    res = requests.get(url)
+    img = Image.open(BytesIO(res.content))
+    img.save(IMAGE_INPUT)
+
+    with mp_face_detection.FaceDetection(
+        model_selection=1, min_detection_confidence=0.5) as face_detection:
+    
+        image = cv2.imread(IMAGE_INPUT)
+        # Convert the BGR image to RGB and process it with MediaPipe Face Detection.
+        results = face_detection.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+        # Draw face detections of each face.
+        if not results.detections:
+            return False
+
+        annotated_image = image.copy()
+        for detection in results.detections:
+            mp_drawing.draw_detection(annotated_image, detection)
+        cv2.imwrite(IMAGE_PATH, annotated_image)
+
+        return True
