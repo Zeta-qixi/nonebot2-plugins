441497d298494ba873a886396b36e4def24f7843
Zeta-qixi <1019289695@qq.com>
2022-03-27 17:06:38 +0800

    clock

---
+++ clock/__init__.py
@@ -0,0 +1,168 @@
+from .db import *
+import os
+from  time import strftime, localtime
+from datetime import datetime, timedelta
+import pandas as pd
+import re
+from .Clock import Clock
+from nonebot import on_command, on_message, get_bot, get_driver
+from nonebot.adapters.onebot.v11.bot import Bot
+from nonebot.adapters.onebot.v11.event import Event, GroupMessageEvent,MessageEvent
+from nonebot.adapters.onebot.v11.message import Message
+from nonebot.params import State, CommandArg
+
+from nonebot.typing import T_State
+from nonebot import require, logger
+
+scheduler = require('nonebot_plugin_apscheduler').scheduler
+
+
+try:
+    master = get_driver().config.master
+except:
+    master = []
+
+CLOCK_DATA = {}
+
+
+def create_clock_scheduler(clock):
+    '''
+    创建闹钟任务
+    '''
+    CLOCK_DATA[clock.id] = clock
+
+    async def add_clock():
+        if clock.verify_today():
+            await get_bot().send_msg(message_type=clock.type, user_id=clock.user, group_id=clock.user, message=clock.content)          
+            if clock.ones == 1:
+                del_clock_db(clock.id)
+                scheduler.remove_job(f"clock_{clock.id}")
+
+    scheduler.add_job(add_clock, "cron", hour=clock.hour, minute=clock.minute, id=f"clock_{clock.id}")
+
+for i in select_all():
+    
+    c = Clock.init_from_db(i)
+    create_clock_scheduler(c)
+
+
+def add_clock(**kwargs):
+    """添加闹钟"""
+    kwargs['id'] = new_id()
+    clock = Clock((kwargs))
+    add_clock_db(clock)
+    create_clock_scheduler(clock)
+
+def del_clock(id):
+    """删除闹钟"""
+
+    del_clock_db(id)
+    scheduler.remove_job(f"clock_{id}")
+
+
+
+def get_time(time_):
+    t = None
+    r = re.match(r'(\d+)[:|\-|：|.](\d+)',time_)
+    if time_.startswith('+'):
+        h = re.search(r"(\d+)[Hh时]",time_)
+        m = re.search(r"(\d+)[Mm分]",time_)
+        h=int(h.groups()[0]) if h else 0
+        m=int(m.groups()[0]) if m else 0
+        t = (datetime.now() + timedelta(hours=h, minutes=m)).strftime("%H:%M")
+
+    elif r:
+        h, m = r.groups()
+        if int(h) < 24 or int(m) < 60:
+            h = f'0{h}' if len(h)==1 else h
+            m = f'0{m}' if len(m)==1 else m
+            t = f'{h}:{m}'
+
+    return t
+        
+
+# 创建闹钟
+add_clock_qq = on_command('添加闹钟', aliases={'设置闹钟', '添加提醒事项', 'addclock'})
+@add_clock_qq.handle()
+async def _(bot: Bot, event: Event, state: T_State, messages:Message = CommandArg()):
+
+    uid = event.user_id
+    type = event.get_event_name()
+    messages = str(messages).split(' ')
+    ones = 1
+    content = '⏰'
+    try:
+        time_ = messages[0]
+        content = messages[1]
+        if messages[2]:
+            ones = 0
+    except:
+        pass
+
+    time_ = get_time(time_)
+    if not time_:
+        await add_clock_qq.finish(message="时间格式错误")
+
+
+    data = {
+        'user' : uid,
+        'content' : content,
+        'time' : time_,
+        'ones' : ones,
+        'type' : 'private'
+    }
+       
+    if 'group' in type:
+        gid = event.group_id
+        info = await bot.get_group_member_info(group_id=gid, user_id=uid)
+        if info['role'] == "member" and uid not in master:
+            await add_clock_qq.finish(message="你没有该权限哦～")
+
+        data['type'] = 'group'
+        data['user'] = gid
+
+
+    add_clock(**data)
+    await add_clock_qq.finish(message="添加成功～")
+
+
+# # 查看闹钟
+# check = on_command('查看闹钟',  aliases={'提醒事项', '闹钟','⏰'})
+# @check.handle()
+# async def add_handle(bot: Bot, event: Event):
+#     try:
+#         id = event.group_id
+#     except:
+#         id = event.user_id
+    
+#     clock_msg = ''
+#     ones=['days', 'ones']
+#     for clock in clock_list:
+#         if clock.user == id:
+
+#             if clock_msg:
+#                 clock_msg = clock_msg + f'\n\n{clock.get_info()}'
+#             else:
+#                 clock_msg = clock_msg + f'{clock.get_info()}'
+#     if clock_msg:
+#         await bot.send(event, message= Message(clock_msg))
+#     else:
+#         await bot.send(event, message='目前没有闹钟')
+    
+
+
+# # 删除闹钟
+# del_ = on_command('删除闹钟')
+# @del_.handle()
+# async def del_handle(bot: Bot, event: Event):
+#     id = str(event.get_message())
+#     if id.isdigit():
+#         if del_clock(int(id)):
+#             await bot.send(event, message='删除成功')
+#             return
+#     await bot.send(event, message='失败了')
+
+
+
+
+
---
+++ clock/db.py
@@ -0,0 +1,52 @@
+
+import os
+import sqlite3
+from .Clock import Clock
+path =os.path.dirname(__file__)
+db = path + '/data.db'
+
+TABLE = "CLOCKS"
+
+def execute(sql:str):
+    conn = sqlite3.connect(db)
+    c = conn.cursor()
+    res = c.execute(sql)
+    res = list(res)
+    conn.commit()
+    conn.close()
+    return res
+
+def add_clock_db(clock: Clock):
+    sql = f'''INSERT INTO {TABLE} (type, user, content, month, day, week, c_time, ones)
+    values ("{clock.type}", {clock.user}, "{clock.content}","{clock.month}","{clock.day}","{clock.week}","{clock.time}",{clock.ones});'''
+    execute(sql)
+
+def del_clock_db(id):
+    execute(f"DELETE from {TABLE} where id = {id}")
+
+def select_all():
+    '''
+    (id, type, user, content, c_time, ones) 
+    '''
+    #DataFrame(data = data, columns=['id', 'type', 'uid', 'note', 'time', 'omes'])
+    return execute(f"SELECT * FROM {TABLE};")
+
+    
+def new_id():
+    res = execute(f"SELECT max(id) FROM {TABLE};")
+    return res[0][0] + 1 if res[0][0] else 0
+
+try:
+    execute(f'''CREATE TABLE {TABLE}(  
+        id INTEGER NOT NULL primary key autoincrement,
+        type CHAR(10),
+        user INTEGER NOT NULL,
+        content VARCHAR(20),
+        month INTEGER,
+        day INTEGER,
+        week VARCHAR(7),
+        c_time TIME,
+        ones INTEGER NOT NULL);
+    ''')
+except:
+    pass
