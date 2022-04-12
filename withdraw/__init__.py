5c3b6fb06c5137a94e708d8c6e0e951fd0453882
Zeta-qixi <1019289695@qq.com>
2022-04-12 09:18:50 +0800

    撤回 debug

--- withdraw/__init__.py
+++ withdraw/__init__.py
@@ -1,7 +1,7 @@
 from nonebot import on_command, get_driver
 from nonebot.adapters.onebot.v11.bot import Bot
 from nonebot.adapters.onebot.v11.event import GroupMessageEvent
-
+from nonebot import logger
 try:
     master =  get_driver().config.master
 except:
@@ -14,12 +14,12 @@ async def _(bot: Bot, event: GroupMessageEvent):
     if event.reply:
         
         try:
-            if event.reply.sender['user_id'] == bot.self_id or event.reply.sender['user_id'] in master:
+            if event.reply.sender.user_id == int(bot.self_id) or event.reply.sender.user_id in master:
                 await bot.delete_msg(message_id=event.reply.message_id)
                 await bot.delete_msg(message_id=event.message_id)
             else:
                 ...
                 # await withdraw.finish(message="你没有权限哦～")
             
-        except:
+        except BaseException as e:
             await withdraw.finish(message="撤回不了哦～")
