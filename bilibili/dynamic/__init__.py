f41d2d3a63b81db330d9d159546ba90bb911f08a
Zeta-qixi <1019289695@qq.com>
2022-11-26 15:12:23 +0800

    合并发送

--- bilibili/dynamic/__init__.py
+++ bilibili/dynamic/__init__.py
@@ -1,5 +1,5 @@
 from .browser import get_dynamic_screenshot
-from .GetData import *
+from .GetData import get_dynamic, Dynamic
 from ..db import *
 
 from nonebot.log import logger
@@ -14,43 +14,33 @@ except:
 
 # (id, gid, mid, name, live, is_live, dynamic, lastest_dynamic, dy_filter) 
 def get_data_from_db():
-    data = []
-    for item in (select_dynamic()):
-        data.append({
-            "gid":item[1], "mid":item[2], "lastest":item[-2], "filter":item[-1]
-        })
-    return data
+
+    return [{"gid":item[1], "mid":item[2], "lastest":item[-2],  
+             "filter":item[-1]} for item in (select_dynamic())]
+
 
 scheduler = require('nonebot_plugin_apscheduler').scheduler
 @scheduler.scheduled_job('cron', minute='*/10', id='dynamic_sched_', )
 async def push_dynamic():
-
-    message = []
+    
     for item in get_data_from_db():
         data = get_dynamic(item['mid'])
-        for dy in data['cards'][3::-1]: # 前3条 倒序
-            
+        
+        msg_pic = ['发布有新的动态～']
+        for dy in data['cards'][3::-1]: # 前3条 倒序 
             dy = Dynamic(dy)
             url = dy.url
-            # 判断是否最新的
-            if item["lastest"] < dy.time:
-                update(item["gid"], item["mid"], 'latest_dynamic', dy.time)
-                if dy.type != 1:
+            if item["lastest"] < dy.time and dy.type != 1: # 判断最新与转发
+                try:
+                    res_list = await get_dynamic_screenshot(url, item['filter'])
+                    msg_pic.append(MessageSegment.image(f"base64://{res_list['dy']}"))
+                    for img in res_list.get('img_url', []):
+                        msg_pic.append(MessageSegment.image(file=img))
+                    update(item["gid"], item["mid"], 'latest_dynamic', dy.time)
+                except Exception as e:
+                    logger.error(repr(e))
                     
-                    try:
-                        res_list = await get_dynamic_screenshot(url, item['filter'])
-                        msg_pic = MessageSegment.image(f"base64://{res_list['dy']}")
-                        message.append(f'发布了新的动态:\n{dy.url}' + msg_pic)
-                       
-                        for img in res_list.get('img_url', []):
-                            msg_pic = MessageSegment.image(file=img)
-                            message.append(msg_pic)
-                            
-                    except Exception as e:
-                        logger.error(repr(e))
-
-    if message:                   
-        await send_forward_msg_group(get_bot(),group_id = item["gid"], message= message)
+        await send_forward_msg_group(get_bot(), group_id = item["gid"], message=msg_pic) 
 
          
 # 测试用
@@ -62,8 +52,8 @@ async def check_dynamic_handle(bot: Bot, event):
         data = get_dynamic(item['mid'])
         dy = data['cards'][0]
         dy = Dynamic(dy)
-        info = dy.get()   
-        url = info[2]
+      
+        url = dy.url
         
         try:
             res_list = await get_dynamic_screenshot(url, item['filter']) 
